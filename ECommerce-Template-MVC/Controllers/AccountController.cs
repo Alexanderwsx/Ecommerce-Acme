a58b57517028859d93c0da9dce5ae61d72b3cd65
Alexanderwsx <roberto.au.vera@gmail.com>
2023-09-26 01:07:36 -0400

    login and register fix

--- ECommerce-Template-MVC/Controllers/AccountController.cs
+++ ECommerce-Template-MVC/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using ECommerce_Template_MVC.Data;
 using ECommerce_Template_MVC.Models;
+using ECommerce_Template_MVC.Models.ViewModel;
 using ECommerce_Template_MVC.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +13,13 @@ namespace ECommerce_Template_MVC.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        public AccountController(UserManager<ApplicationUser> userManager, ApplicationDbContext context, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _context = context;
+            _signInManager = signInManager;
         }
         public IActionResult Register()
         {
@@ -45,6 +49,46 @@ namespace ECommerce_Template_MVC.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            // Si el modelo es válido, procedemos con el intento de inicio de sesión
+            if (ModelState.IsValid)
+            {
+                // Intentamos iniciar sesión con las credenciales proporcionadas
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+               
+                if (result.Succeeded)
+                {
+                    // Si el inicio de sesión es exitoso, redirigimos al usuario a la página principal o a la URL de retorno (si se proporciona)
+                    if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction(nameof(HomeController.Index), "Home");
+                    }
+                }
+                //else if (result.IsLockedOut)
+                //{
+                //    // Si la cuenta del usuario está bloqueada, puedes redirigirlo a una página específica o mostrar un mensaje
+                //    return RedirectToAction(nameof(Lockout));
+                //}
+                else
+                {
+                    // Si el inicio de sesión falla, mostramos un mensaje de error y volvemos a mostrar el formulario
+                    ModelState.AddModelError(string.Empty, "Intento de inicio de sesión no válido.");
+                    return View(model);
+                }
+            }
+
+            // Si llegamos hasta aquí, algo falló, volver a mostrar el formulario
+            return View(model);
         }
     }
 }
